79dd2aa17fa77e91d444adef2f3a5014772656dd
Victor <viictorawayz@gmail.com>
2022-02-18 22:14:41 -0300

    📚 Added annotations
    Some annotations has been added to make the code easier to read

--- capgemini-first-question/src/main/java/com/capgemini/academy/FirstQuestionMain.java
+++ capgemini-first-question/src/main/java/com/capgemini/academy/FirstQuestionMain.java
@@ -17,16 +17,26 @@ public class FirstQuestionMain {
     */
 
     public static void main(String[] args) {
+        /*
+          Instanciando o Scanner e utilizando o recurso "try-with-resource"
+          (classes que implementam a classe Closeable conseguem utilizar este recurso e fechar automaticamente após o uso)
+         */
         try (final Scanner scanner = new Scanner(System.in)) {
+            // Instanciando a quantia de degraus da escada
             final int ladderCount = scanner.nextInt();
 
+            // Instanciando um StringBuilder
             final StringBuilder builder = new StringBuilder();
             for (int step = 1; step < ladderCount + 1; step++)
+                // Adicionando à String uma repetição de "degraís total - degrau atual" e uma outra repetição do degrau atual
                 builder.append(" ".repeat(ladderCount - step))
                         .append("*".repeat(step))
                         .append("\n");
 
+            // Instaciando o resultado e removendo a última letra que seria o quebra linha
             final String result = builder.substring(0, builder.length() - 1);
+
+            // Imprimindo o resultado
             System.out.println(result);
         }
     }
